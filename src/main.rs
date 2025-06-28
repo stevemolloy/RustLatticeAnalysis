66159cfe68d61bfe336e7a80da7e86a8b41dde5f
Stephen Molloy <sdmolloy@gmail.com>
2025-06-28 12:50:10 +0200

    Only one Element type. No need for traits.

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,190 @@
 use ndarray::Array2;
 
-pub trait Element {
-    fn length(&self) -> f64;
+#[derive(Debug)]
+pub enum EleType {
+    EleTypeMarker,
+    EleTypeDrift,
+    EleTypeBend,
+    EleTypeQuad,
+    EleTypeSext,
+    EleTypeOct,
+    EleTypeMult,
 }
 
-#[derive(Default)]
-pub struct Drift {
-    l: f64,
-    r_mat: Array2<f64>,
+#[derive(Debug)]
+pub struct Element<'a> {
+    name: &'a str,
+    length: f64,
+    k: [f64; 4],
+    r_matrix: Array2<f64>,
 }
 
-pub struct Quad {
-    l: f64,
-    k1: f64,
-    r_mat: Array2<f64>,
+impl<'a> Default for Element<'a> {
+    fn default() -> Self {
+        Self {
+            name: "",
+            length: 0.0,
+            k: [0.0; 4],
+            r_matrix: Array2::eye(6),
+        }
+    }
 }
 
-pub struct Sbend {
-    l: f64,
-    angle: f64,
-    k1: f64,
-    e1: f64,
-    e2:f64,
-    r_mat: Array2<f64>,
+pub fn element_type(ele: &Element) -> EleType {
+    if ele.length == 0.0 {
+        return EleType::EleTypeMarker;
+    } else if ele.k[0] == 0.0 && ele.k[1] == 0.0 && ele.k[2] == 0.0 && ele.k[3] == 0.0 {
+        return EleType::EleTypeDrift;
+    } else if ele.k[0] != 0.0 && ele.k[2] == 0.0 && ele.k[3] == 0.0 {
+        return EleType::EleTypeBend;
+    } else if ele.k[0] == 0.0 && ele.k[1] != 0.0 && ele.k[2] == 0.0 && ele.k[3] == 0.0 {
+        return EleType::EleTypeQuad;
+    } else if ele.k[0] == 0.0 && ele.k[1] == 0.0 && ele.k[2] != 0.0 && ele.k[3] == 0.0 {
+        return EleType::EleTypeSext;
+    } else if ele.k[0] == 0.0 && ele.k[1] == 0.0 && ele.k[2] == 0.0 && ele.k[3] != 0.0 {
+        return EleType::EleTypeOct;
+    } else {
+        return EleType::EleTypeMult;
+    }
 }
 
-pub struct Mult {
-    l: f64,
-    k1l: f64,
-    k2l: f64,
-    k3l: f64,
-    r_mat: Array2<f64>,
+pub fn make_marker(name: &str) -> Element {
+    Element { name, ..Default::default() }
 }
 
-pub struct Sext {
-    l: f64,
-    k2: f64,
-    r_mat: Array2<f64>,
+pub fn make_drift(name: &str, length: f64) -> Element {
+    let mut r_matrix = Array2::eye(6);
+    r_matrix[[0,1]] = length;
+    r_matrix[[2,3]] = length;
+    Element {
+        name,
+        length,
+        r_matrix,
+        ..Default::default()
+    }
 }
 
-pub struct Oct {
-    l: f64,
-    k3: f64,
-    r_mat: Array2<f64>,
-}
-
-impl Element for Drift {
-    fn length(&self) -> f64 { self.l }
-}
-
-impl Drift {
-    fn make_mat(&mut self) {
-        self.r_mat = eye(6);
+pub fn make_quad(name: &str, length: f64, k1: f64) -> Element {
+    if length == 0.0 {
+        return make_marker(name);
     }
 
+    if k1 == 0.0 {
+        return make_drift(name, length);
+    }
+
+    let omega: f64 = k1.abs().sqrt();
+    let omega_l: f64 = omega * length;
+
+    let c_omega_l: f64 = omega_l.cos();
+    let s_omega_l: f64 = omega_l.sin();
+    let ch_omega_l: f64 = omega_l.cosh();
+    let sh_omega_l: f64 = omega_l.sinh();
+
+    let mut r_matrix = Array2::eye(6);
+
+    if k1 > 0.0 {
+        // Focusing 2x2
+        r_matrix[[0,0]] = c_omega_l;
+        r_matrix[[0,1]] = s_omega_l / omega;
+        r_matrix[[1,0]] = s_omega_l * (-omega);
+        r_matrix[[1,1]] = c_omega_l;
+        // Defocusing 2x2
+        r_matrix[[2,2]] = ch_omega_l;
+        r_matrix[[2,3]] = sh_omega_l / omega;
+        r_matrix[[3,2]] = sh_omega_l * omega;
+        r_matrix[[3,3]] = ch_omega_l;
+    } else {
+        // Focusing 2x2
+        r_matrix[[2,2]] = c_omega_l;
+        r_matrix[[2,3]] = s_omega_l / omega;
+        r_matrix[[3,2]] = s_omega_l * (-omega);
+        r_matrix[[3,3]] = c_omega_l;
+        // Defocusing 2x2
+        r_matrix[[0,0]] = ch_omega_l;
+        r_matrix[[0,1]] = sh_omega_l / omega;
+        r_matrix[[1,0]] = sh_omega_l * omega;
+        r_matrix[[1,1]] = ch_omega_l;
+    }
+
+    Element { name, length, k: [0.0, k1, 0.0, 0.0], r_matrix, ..Default::default() }
 }
 
-impl Element for Quad {
-    fn length(&self) -> f64 { self.l }
-}
+pub fn make_sbend(name: &str, length: f64, angle: f64, k1: f64) -> Element {
+    if length == 0.0 {
+        return make_marker(name);
+    }
+    if angle == 0.0 {
+        return make_quad(name, length, k1);
+    }
 
-impl Element for Sbend {
-    fn length(&self) -> f64 { self.l }
-}
+    let h = angle / length;
 
-impl Element for Sext {
-    fn length(&self) -> f64 { self.l }
-}
+    let omega_x_sqr = h.powi(2) + k1;
+    println!("{omega_x_sqr}");
+    let omega_x = omega_x_sqr.abs().sqrt();
+    let omega_x_l = omega_x * length;
 
-impl Element for Oct {
-    fn length(&self) -> f64 { self.l }
-}
+    let omega_y_sqr = k1;
+    let omega_y = omega_y_sqr.abs().sqrt();
+    let omega_y_l = omega_y * length;
 
-impl Element for Mult {
-    fn length(&self) -> f64 { self.l }
+    let mut r_matrix = Array2::eye(6);
+
+    if omega_x_sqr == 0.0 {
+        r_matrix[[0,1]] = length;
+        r_matrix[[4,0]] = h * length;
+    } else if omega_x_sqr > 0.0 {
+        r_matrix[[0,0]] = omega_x_l.cos();
+        r_matrix[[0,1]] = omega_x_l.sin() / omega_x;
+        r_matrix[[1,0]] = omega_x_l.sin() * (-omega_x);
+        r_matrix[[1,1]] = omega_x_l.cos();
+        r_matrix[[0,5]] = (h/omega_x_sqr) * (1.0 - omega_x_l.cos());
+        r_matrix[[1,5]] = (h/omega_x) * omega_x_l.sin();
+        r_matrix[[4,0]] = r_matrix[[1,5]];
+        r_matrix[[4,1]] = r_matrix[[0,5]];
+        r_matrix[[4,5]] = -h.powi(2) * (omega_x_l - omega_x_l.sin()) / omega_x.powi(3);
+    } else {
+        r_matrix[[0,0]] = omega_x_l.cosh();
+        r_matrix[[0,1]] = omega_x_l.sinh() / omega_x;
+        r_matrix[[1,0]] = omega_x_l.sinh() * (-omega_x);
+        r_matrix[[1,1]] = omega_x_l.cosh();
+        r_matrix[[0,5]] = (h/omega_x_sqr) * (1.0 - omega_x_l.cosh());
+        r_matrix[[1,5]] = (h/omega_x) * omega_x_l.sinh();
+        r_matrix[[4,0]] = r_matrix[[1,5]];
+        r_matrix[[4,1]] = r_matrix[[0,5]];
+        r_matrix[[4,5]] = -h.powi(2) * (omega_x_l - omega_x_l.sinh()) / omega_x.powi(3);
+    }
+
+    if omega_y_sqr == 0.0 {
+        r_matrix[[0,1]] = length;
+        r_matrix[[4,0]] = h * length;
+    } else if omega_y_sqr > 0.0 {
+        r_matrix[[2,2]] = omega_y_l.cos();
+        r_matrix[[2,3]] = omega_y_l.sin() / omega_y;
+        r_matrix[[3,2]] = omega_y_l.sin() * (-omega_y);
+        r_matrix[[3,3]] = omega_y_l.cos();
+    } else {
+        r_matrix[[2,2]] = omega_y_l.cosh();
+        r_matrix[[2,3]] = omega_y_l.sinh() / omega_y;
+        r_matrix[[3,2]] = omega_y_l.sinh() * (-omega_y);
+        r_matrix[[3,3]] = omega_y_l.cosh();
+    }
+
+    Element { name, length, k: [angle, k1, 0.0, 0.0], r_matrix, ..Default::default() }
 }
 
 fn main() {
-    let s1: Sext = Sext{l: 1.5, k2: 9.6};
-    println!("s1.l = {l}", l=s1.l);
+    let q1 = make_quad("quad", 1.1, -0.5);
+    let d1 = make_drift("drift", 2.0);
+    let m1 = make_marker("marker");
+    let s1 = make_sbend("sext", 2.1, 0.1, 0.0);
 
-    // let line: Vec<Box<dyn Element>> = vec![Box::new(s1)];
+    println!("{name} :: {t:?}", name = q1.name, t = element_type(&q1));
+    println!("{name} :: {t:?}", name = d1.name, t = element_type(&d1));
+    println!("{name} :: {t:?}", name = m1.name, t = element_type(&m1));
+
+    println!("{d1:?}");
+    println!("{s1:?}");
 }
+
