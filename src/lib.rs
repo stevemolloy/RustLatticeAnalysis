e77ad16f572989bed8d3ee8028e727fd57c14170
Stephen Molloy <sdmolloy@gmail.com>
2025-06-29 15:17:45 +0200

    Starting to write the parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use ndarray::Array2;
 use std::fmt::{Display, Error, Formatter};
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::process::exit;
 
 #[derive(Debug)]
 pub enum EleType {
@@ -237,4 +240,22 @@ pub fn get_line_matrix(line: &[Element]) -> Array2<f64> {
     }
 
     return retval;
+}
+
+pub fn parse_lattice_from_tracy_file(file_path: &str) -> Result<Vec<Element>, ()> {
+    let f = File::open(file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: Could not open {file_path}: {err}");
+        exit(1);
+    });
+    let mut reader = BufReader::new(f);
+
+    let mut file_contents: String = String::new();
+    reader
+        .read_to_string(&mut file_contents)
+        .unwrap_or_else(|err| {
+            eprintln!("ERROR: Could not open {file_path}: {err}");
+            exit(1);
+        });
+
+    todo!();
 }
--- tests/element_tests.rs
+++ tests/element_tests.rs
@@ -1,14 +1,20 @@
 use rust_lattice_analysis::*;
 
 #[test]
-fn test_line_matrix() {
-    let q1 = make_quad("quad".to_string(), 1.1, -0.5);
-    let d1 = make_drift("drift".to_string(), 2.0);
-    let m1 = make_marker("marker".to_string());
-    let s1 = make_sbend("sext".to_string(), 2.1, 0.1, 0.0);
-
-    let line = [d1, q1, m1, s1];
-    let line_matrix = get_line_matrix(&line);
-
-    println!("{line_matrix:?}");
+fn test_read_lattice_from_file() {
+    let file_path = "./lattices/max_4u_sp_jb_5.lat";
+    let line = parse_lattice_from_tracy_file(file_path);
 }
+
+// #[test]
+// fn test_line_matrix() {
+//     let q1 = make_quad("quad".to_string(), 1.1, -0.5);
+//     let d1 = make_drift("drift".to_string(), 2.0);
+//     let m1 = make_marker("marker".to_string());
+//     let s1 = make_sbend("sext".to_string(), 2.1, 0.1, 0.0);
+//
+//     let line = [d1, q1, m1, s1];
+//     let line_matrix = get_line_matrix(&line);
+//
+//     println!("{line_matrix:?}");
+// }
