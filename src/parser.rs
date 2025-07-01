29f3c1cc3dcbea54cc632fbe94eceedff719da4e
Stephen Molloy <sdmolloy@gmail.com>
2025-07-01 14:38:58 +0200

    Parsing nearly fully developed. With tests.

--- src/parser.rs
+++ src/parser.rs
@@ -2,8 +2,9 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::process::exit;
 
+use winnow::combinator::{delimited, separated};
 use winnow::token::literal;
-use winnow::token::take_while;
+use winnow::token::{take_till, take_while};
 use winnow::{Parser, Result};
 
 use crate::element::Element;
@@ -30,18 +31,64 @@ pub fn do_nothing_parser<'a>(_input: &mut &'a str) -> Result<&'a str> {
     Ok("")
 }
 
+pub fn parse_optional_whitespace<'a>(input: &mut &'a str) -> Result<&'a str> {
+    take_while(0.., char::is_whitespace).parse_next(input)
+}
+
 pub fn parse_symbol<'a>(input: &mut &'a str) -> Result<&'a str> {
     take_while(1.., |c: char| c.is_alphanumeric() || c == '_').parse_next(input)
 }
 
 pub fn use_line_parser<'a>(input: &mut &'a str) -> Result<&'a str> {
     (
-        literal("USE"),
-        literal(":"),
-        take_while(1.., char::is_whitespace),
+        literal("USE:"),
+        parse_optional_whitespace,
         parse_symbol,
         literal(";"),
     )
-        .map(|(_, _, _, sym, _)| sym)
+        .map(|(_, _, sym, _)| sym)
+        .parse_next(input)
+}
+
+pub fn expr_til_semicolon_or_comma<'a>(input: &mut &'a str) -> Result<&'a str> {
+    take_till(1.., |c| c == ';' || c == ',').parse_next(input)
+}
+
+pub fn variable_assignment_parser<'a>(input: &mut &'a str) -> Result<(&'a str, &'a str)> {
+    (
+        parse_symbol,
+        parse_optional_whitespace,
+        literal("="),
+        parse_optional_whitespace,
+        expr_til_semicolon_or_comma,
+    )
+        .map(|(sym, _, _, _, expr)| (sym, expr))
+        .parse_next(input)
+}
+
+pub fn element_creation_parser<'a>(
+    input: &mut &'a str,
+) -> Result<(&'a str, &'a str, Vec<(&'a str, &'a str)>)> {
+    (
+        parse_symbol,
+        parse_optional_whitespace,
+        literal(":"),
+        parse_optional_whitespace,
+        parse_symbol,
+        parse_optional_whitespace,
+        literal(","),
+        parse_optional_whitespace,
+        separated(
+            0..,
+            variable_assignment_parser,
+            delimited(
+                parse_optional_whitespace,
+                literal(","),
+                parse_optional_whitespace,
+            ),
+        ),
+        literal(";"),
+    )
+        .map(|(sym, _, _, _, typ, _, _, _, fields, _)| (sym, typ, fields))
         .parse_next(input)
 }
--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -19,3 +19,69 @@ fn test_use_line_parser() {
     assert_eq!(input, "");
     assert_eq!(output, Ok("line_to_use"));
 }
+
+#[test]
+fn test_parse_till_semicolon_or_comma() {
+    let mut input = "12 + 15 / -42;";
+    let output = expr_til_semicolon_or_comma(&mut input);
+    assert_eq!(input, ";");
+    assert_eq!(output, Ok("12 + 15 / -42"));
+
+    let mut input = "12e+01 + 15e-5 / -42,";
+    let output = expr_til_semicolon_or_comma(&mut input);
+    assert_eq!(input, ",");
+    assert_eq!(output, Ok("12e+01 + 15e-5 / -42"));
+}
+
+#[test]
+fn test_variable_assignment_parser() {
+    let mut input = "L =  20 + 2;";
+    let output = variable_assignment_parser(&mut input);
+    assert_eq!(input, ";");
+    assert_eq!(output, Ok(("L", "20 + 2")));
+
+    let mut input = "my_element =  20 - 14e-6 / 2,";
+    let output = variable_assignment_parser(&mut input);
+    assert_eq!(input, ",");
+    assert_eq!(output, Ok(("my_element", "20 - 14e-6 / 2")));
+
+    let mut input = "my_element=20,";
+    let output = variable_assignment_parser(&mut input);
+    assert_eq!(input, ",");
+    assert_eq!(output, Ok(("my_element", "20")));
+}
+
+#[test]
+fn test_element_creation_parser() {
+    let mut input = "d8:  Drift, L = 0.125 - 0.1;";
+    let output = element_creation_parser(&mut input);
+    assert_eq!(input, "");
+    assert_eq!(output, Ok(("d8", "Drift", vec![("L", "0.125 - 0.1")])));
+
+    let mut input = "q1   : Quadrupole, L = 0.25000, Phi =  0.00000, B_2 =  4.79596, N = n_quad;";
+    let output = element_creation_parser(&mut input);
+    assert_eq!(input, "");
+    assert_eq!(
+        output,
+        Ok((
+            "q1",
+            "Quadrupole",
+            vec![
+               ("L","0.25000"),
+               ("Phi","0.00000"),
+               ("B_2","4.79596"),
+               ("N","n_quad")
+            ]
+        ))
+    );
+
+    let mut input = "cav: Cavity, Frequency = c0/C*h_rf, Voltage = 2*1.50e6, HarNum = h_rf,\n     Phi = 0.0;";
+    let output = element_creation_parser(&mut input);
+    assert_eq!(input, "");
+    assert_eq!(output, Ok(("cav", "Cavity", vec![
+                ("Frequency", "c0/C*h_rf"),
+                ("Voltage", "2*1.50e6"),
+                ("HarNum", "h_rf"),
+                ("Phi", "0.0"),
+    ])));
+}
