7e7d7b77a2313457255ef4ae6217fa06754f0885
Stephen Molloy <sdmolloy@gmail.com>
2025-07-10 22:43:47 +0200

    Fixed the tests

--- tests/line_tests.rs
+++ tests/line_tests.rs
@@ -11,13 +11,18 @@ fn max_lattice_test() {
         1.563961e-3,
         2.092111e-2,
         1.061231e-3,
-        2.540279e-2,
+        -2.540279e-2,
         1.887260e-7,
     ];
+    let emit_exp = 53.808e-12;
+    let espread_exp = 9.234e-4;
 
-    assert!((line.synch_integrals[0] - si_exp[0]) / si_exp[0] < 1e-6);
-    assert!((line.synch_integrals[1] - si_exp[1]) / si_exp[1] < 1e-6);
-    assert!((line.synch_integrals[2] - si_exp[2]) / si_exp[2] < 1e-6);
-    assert!((line.synch_integrals[3] - si_exp[3]) / si_exp[3] < 1e-6);
-    assert!((line.synch_integrals[4] - si_exp[4]) / si_exp[4] < 1e-6);
+    assert!(((line.synch_integrals[0] - si_exp[0]) / si_exp[0]).abs() < 1e-6);
+    assert!(((line.synch_integrals[1] - si_exp[1]) / si_exp[1]).abs() < 1e-6);
+    assert!(((line.synch_integrals[2] - si_exp[2]) / si_exp[2]).abs() < 1e-6);
+    assert!(((line.synch_integrals[3] - si_exp[3]) / si_exp[3]).abs() < 1e-6);
+    assert!(((line.synch_integrals[4] - si_exp[4]) / si_exp[4]).abs() < 1e-6);
+
+    assert!(((emit_exp - line.nat_emitt_x) / emit_exp).abs() < 1e-3);
+    assert!(((espread_exp - line.e_spread) / espread_exp).abs() < 1e-3);
 }
